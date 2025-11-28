b628fd031fc1032bd681e39575dcb1961f66299b
linkwithabdullah <abdullahkhan84328@gmail.com>
2025-11-28 17:20:14 +0500

    Added assignment 13 javascript equation

---
+++ All Assignments/assig-13-javascript/app.js
@@ -0,0 +1,16 @@
+// Please resolve this equation step by step using js
+
+// 18÷(3+3)×4−{12−[5×(2+1)]}+7^2
+
+var rb1 = 3 + 3;
+var division = 18 / rb1;
+var multiply = division * 4;
+
+var rb2 = 2 + 1;
+var sb = 5 * rb2;
+var cb = 12 - sb;
+
+var power =  7 ** 2;
+var result = multiply - cb + power;
+
+console.log(result); // result is 64
