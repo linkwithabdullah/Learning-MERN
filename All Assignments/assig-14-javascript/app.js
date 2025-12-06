46c942a1d36b387f9409346a22eb4145ad790dd3
Abdullah Khan <97393959+linkwithabdullah@users.noreply.github.com>
2025-12-06 19:13:26 +0500

    Added Assignment 14 JavaScript Prompt

---
+++ All Assignments/assig-14-javascript/app.js
@@ -0,0 +1,14 @@
+// Take the user name in prompt
+// Take the user age in prompt
+
+// Show the alert saying `Congratulations! Abdullah, You're 25 year old`
+// name and age will be shown as per user given values
+
+
+
+var myName = prompt("Enter your name:");
+var myAge = prompt("Enter your age:");
+
+console.log(myName, myAge);
+
+alert("Hello " + myName + ", you are " + myAge + " years old!");
